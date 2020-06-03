b3d1fbe9b546c4bf89c0797f5e02afd5503ee1e5
Paulo César <pdesignerpb@gmail.com>
2020-06-03 12:14:14 -0300

    user can list all items or him can create | read a point.

--- Backend/src/routes.ts
+++ Backend/src/routes.ts
@@ -1,9 +1,14 @@
 import { Router } from "express";
+import knex from "./database/connection";
 
 const routes = Router();
 
-routes.get("/users", (req, res) => {
-  return res.json({ ok: true });
-});
+import PointsController from "./controllers/PointsController";
+import ItemsController from "./controllers/ItemsController";
+
+routes.get("/items", ItemsController.index);
+
+routes.get("/points/:id", PointsController.show);
+routes.post("/points", PointsController.store);
 
 export default routes;
